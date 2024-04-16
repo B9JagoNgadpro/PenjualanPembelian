1213217fd84234c136a4e8bd967e74b4ab079ffa
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-16 22:36:13 +0700

    [GREEN] Implement GetGame Endpoint

--- src/main/java/jagongadpro/penjualanpembelian/controller/GameController.java
+++ src/main/java/jagongadpro/penjualanpembelian/controller/GameController.java
@@ -2,6 +2,7 @@ package jagongadpro.penjualanpembelian.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class GameController {
     @PostMapping(value = "/api/games")
     public ResponseEntity<String> createProductPost(){
+        System.out.println("masukkk");
+        return ResponseEntity.ok().body("Hello World");
+    }
+
+    @GetMapping(value = "/api/games/all")
+    public ResponseEntity<String> GetAllProducts(){
         System.out.println("masukkk");
         return ResponseEntity.ok().body("Hello World");
     }
