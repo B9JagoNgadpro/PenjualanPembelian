f0b24cd95570825f8366ab78522cc14359ca8a3a
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-16 17:34:30 +0700

    initializing springboot project

---
+++ src/main/java/jagongadpro/penjualanpembelian/PenjualanPembelianApplication.java
@@ -0,0 +1,13 @@
+package jagongadpro.penjualanpembelian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenjualanPembelianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenjualanPembelianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jagongadpro/penjualanpembelian/PenjualanPembelianApplicationTests.java
@@ -0,0 +1,13 @@
+package jagongadpro.penjualanpembelian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenjualanPembelianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
