17cf5667cb7f484fc8305d8c65951bd4a448d29f
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-18 15:26:55 +0700

    [GREEN] Implement error handler controller for POST /api/games

---
+++ src/main/java/jagongadpro/penjualanpembelian/controller/ErrorController.java
@@ -0,0 +1,17 @@
+package jagongadpro.penjualanpembelian.controller;
+
+import jagongadpro.penjualanpembelian.dto.WebResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorController {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<WebResponse<String>> constraintViolationException(ConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(WebResponse.<String>builder().errors(exception.getMessage()).build());
+    }
+}
