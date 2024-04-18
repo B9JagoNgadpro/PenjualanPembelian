b5d67b53cec748925b760cc9e57965631b6b8c96
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-18 15:25:54 +0700

    [GREEN] implement controller for POST /api/games

--- src/main/java/jagongadpro/penjualanpembelian/controller/GameController.java
+++ src/main/java/jagongadpro/penjualanpembelian/controller/GameController.java
@@ -1,17 +1,30 @@
 package jagongadpro.penjualanpembelian.controller;
 
+import jagongadpro.penjualanpembelian.dto.CreateGameRequest;
+import jagongadpro.penjualanpembelian.dto.WebResponse;
+import jagongadpro.penjualanpembelian.service.GameService;
+import jagongadpro.penjualanpembelian.service.GameServiceImpl;
+import jagongadpro.penjualanpembelian.service.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+
+@RestController
 public class GameController {
-    @PostMapping(value = "/api/games")
-    public ResponseEntity<String> createProductPost(){
+    @Autowired
+    GameService gameService;
 
-        return ResponseEntity.ok().body("Hello World");
+    @PostMapping(value = "/api/games", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<String> createProductPost(@RequestBody CreateGameRequest request){
+        gameService.create(request);
+        return WebResponse.<String>builder().data("OK").build();
     }
 
     @GetMapping(value = "/api/games/all")
