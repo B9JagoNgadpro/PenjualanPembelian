241dd6a9a82df6333ead629e7d5337d9b6a481e4
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-18 15:25:01 +0700

    [RED] Test for POST /api/games

--- src/test/java/jagongadpro/penjualanpembelian/controller/GameControllerTest.java
+++ src/test/java/jagongadpro/penjualanpembelian/controller/GameControllerTest.java
@@ -1,10 +1,16 @@
 package jagongadpro.penjualanpembelian.controller;
+import com.fasterxml.jackson.core.type.TypeReference;
+import jagongadpro.penjualanpembelian.dto.CreateGameRequest;
+import jagongadpro.penjualanpembelian.dto.WebResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.MockMvcBuilder.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -14,13 +20,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 public class GameControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Test
+    public void testCreateGameSuccess() throws Exception {
+        CreateGameRequest request = new CreateGameRequest();
+        request.setNama("Game1");
+        request.setDeskripsi("deskripsi");
+        request.setStok(10);
+        request.setHarga(10000);
+        mockMvc.perform(post("/api/games").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(status().isOk())
+                .andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                    });
+
+                    assertEquals("OK", response.getData());
+                });
+    }
 
     @Test
-    public void testCreateGame() throws Exception {
-        mockMvc.perform(post("/api/games"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+    public void testCreateGameFailed() throws Exception {
+        CreateGameRequest request = new CreateGameRequest();
+        request.setDeskripsi("deskripsi");
+        request.setHarga(10000);
+        request.setStok(100);
+        mockMvc.perform(post("/api/games").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(status().isBadRequest())
+                .andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                    });
 
+                    assertEquals("nama: must not be blank", response.getErrors());
+                });
     }
 
     @Test
