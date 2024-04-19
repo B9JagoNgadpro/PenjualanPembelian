9ba267d1f0d59546ce6aa01dea171c8bd7d020df
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-19 20:41:32 +0700

    [GREEN] Implement game response

---
+++ src/main/java/jagongadpro/penjualanpembelian/dto/GameResponse.java
@@ -0,0 +1,24 @@
+package jagongadpro.penjualanpembelian.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameResponse {
+    private String id;
+
+    private String nama;
+
+    private String deskripsi;
+
+
+    private Integer harga;
+
+    private String kategori;
+
+
+    private Integer stok;
+}
