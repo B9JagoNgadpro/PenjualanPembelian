a3b2cb9776bc628548561acaca21c401defc5e09
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-20 21:16:42 +0700

    [GREEN] extends JPASpecification for implement filter method in service

--- src/main/java/jagongadpro/penjualanpembelian/repository/GameRepository.java
+++ src/main/java/jagongadpro/penjualanpembelian/repository/GameRepository.java
@@ -3,8 +3,9 @@ package jagongadpro.penjualanpembelian.repository;
 import jagongadpro.penjualanpembelian.model.Game;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game,String> {
+public interface GameRepository extends JpaRepository<Game,String>, JpaSpecificationExecutor<Game> {
 }
