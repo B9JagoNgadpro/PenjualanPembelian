5673ed458cb31002b8155cb851bd8ae9ced4cb5e
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-29 09:34:35 +0700

    [GREEN] implement error controller for response status exception

--- src/main/java/jagongadpro/penjualanpembelian/controller/ErrorController.java
+++ src/main/java/jagongadpro/penjualanpembelian/controller/ErrorController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class ErrorController {
@@ -13,5 +14,11 @@ public class ErrorController {
     public ResponseEntity<WebResponse<String>> constraintViolationException(ConstraintViolationException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(WebResponse.<String>builder().errors(exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<WebResponse<String>> responseStatusException(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(WebResponse.<String>builder().errors(exception.getReason()).build());
     }
 }
