f81b82843e1951d198454170e575597253d764ec
Naomi <gabriella.naomi21@ui.ac.id>
2024-05-26 15:20:38 +0700

    [RED] add test to add id penjual of game

--- src/test/java/jagongadpro/penjualanpembelian/model/GameTest.java
+++ src/test/java/jagongadpro/penjualanpembelian/model/GameTest.java
@@ -8,8 +8,9 @@ public class GameTest {
 
     @Test
     void MakeGameBuilder(){
-        Game game1 = new Game.GameBuilder().nama("Game1").stok(10).harga(1000).kategori("action").deskripsi("bagus").build();
+        Game game1 = new Game.GameBuilder().nama("Game1").stok(10).harga(1000).kategori("action").deskripsi("bagus").idPenjual("id").build();
         assertEquals("Game1", game1.getNama());
+        assertEquals("id", game1.getIdPenjual());
         assertEquals(1000, game1.getHarga());
         assertEquals(10, game1.getStok());
         assertEquals("action", game1.getKategori());
